36007939ae3c456316f26590bb5649695bcd052c
Sunl1t <145154426+Sunl1t@users.noreply.github.com>
2023-12-19 23:07:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void InputFirstSymbol()
+    {
+        Console.Write("Ââåäèòå ïåðâûé ñèìâîë: ");
+        ch1 = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+    }
+
+    public static void InputSecondSymbol()
+    {
+        Console.Write("Ââåäèòå âòîðîé ñèìâîë: ");
+        ch2 = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+    }
+
+    public static void PrintSymbolsAndAsciiCodes()
+    {
+        Console.WriteLine("Ñèìâîë " + ch1 + " èìååò êîä ASCII " + (int)ch1);
+        Console.WriteLine("Ñèìâîë " + ch2 + " èìååò êîä ASCII " + (int)ch2);
+    }
+
+    public static void PrintSumOfAsciiCodes()
+    {
+        int sum = (int)ch1 + (int)ch2;
+        Console.WriteLine("Ñóììà êîäîâ ASCII ñèìâîëîâ " + ch1 + " è " + ch2 + " ðàâíà " + sum);
+    }
+
+    public static char ch1 = ' '; // ïåðåìåííàÿ äëÿ õðàíåíèÿ ïåðâîãî ñèìâîëà
+    public static char ch2 = ' '; // ïåðåìåííàÿ äëÿ õðàíåíèÿ âòîðîãî ñèìâîëà
+
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Ìåíþ âûáîðà:");
+            Console.WriteLine("1. Ââåñòè ïåðâûé ñèìâîë");
+            Console.WriteLine("2. Ââåñòè âòîðîé ñèìâîë");
+            Console.WriteLine("3. Âûâåñòè êàæäûé ñèìâîë è åãî êîä ASCII");
+            Console.WriteLine("4. Âûâåñòè ñóììó êîäîâ ASCII ñèìâîëîâ ch1 è ch2");
+            Console.WriteLine("0. Âûéòè");
+
+            Console.Write("Âûáåðèòå ïóíêò ìåíþ: ");
+            int choice = Int32.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 0:
+                    return; // âûõîä èç ïðîãðàììû
+                case 1:
+                    InputFirstSymbol();
+                    break;
+                case 2:
+                    InputSecondSymbol();
+                    break;
+                case 3:
+                    PrintSymbolsAndAsciiCodes();
+                    break;
+            case 4:
+                    PrintSumOfAsciiCodes();
+                    break;
+                default:
+                    Console.WriteLine("Íåêîððåêòíûé âûáîð. Ïîïðîáóéòå ñíîâà.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+class Test
+{
+    public void InputFirstSymbol_InputValidSymbol_ReturnsValidChar()
+    {
+        // Arrange
+        var expected = 'A';
+        var input = new StringReader("A");
+        Console.SetIn(input);
+
+        // Act
+        Program.InputFirstSymbol();
+        var actual = Program.ch1;
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    
+    public void InputSecondSymbol_InputValidSymbol_ReturnsValidChar()
+    {
+        // Arrange
+        var expected = 'B';
+        var input = new StringReader("B");
+        Console.SetIn(input);
+
+        // Act
+        Program.InputSecondSymbol();
+        var actual = Program.ch2;
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    
+    public void InputFirstSymbol_InputInvalidSymbol_ReturnsWhitespaceChar()
+    {
+        // Arrange
+        var expected = ' ';
+        var input = new StringReader("123");
+        Console.SetIn(input);
+
+        // Act
+        Program.InputFirstSymbol();
+        var actual = Program.ch1;
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+
+
+    
+    public void PrintSymbolsAndAsciiCodes_BothSymbolsSet_OutputCorrectMessage()
+    {
+        // Arrange
+        var expectedOutput = "Ñèìâîë A èìååò êîä ASCII 65\r\nÑèìâîë B èìååò êîä ASCII 66\r\n";
+        var output = new StringWriter();
+        Console.SetOut(output);
+
+        Program.ch1 = 'A';
+        Program.ch2 = 'B';
+
+        // Act
+        Program.PrintSymbolsAndAsciiCodes();
+        var actualOutput = output.ToString();
+
+        // Assert
+        Assert.AreEqual(expectedOutput, actualOutput);
+    }
+
+    
+    public void PrintSumOfAsciiCodes_BothSymbolsSet_OutputCorrectMessage()
+    {
+        // Arrange
+        var expectedOutput = "Ñóììà êîäîâ ASCII ñèìâîëîâ A è B ðàâíà 131\r\n";
+        var output = new StringWriter();
+        Console.SetOut(output);
+
+        Program.ch1 = 'A';
+        Program.ch2 = 'B';
+
+        // Act
+        Program.PrintSumOfAsciiCodes();
+        var actualOutput = output.ToString();
+
+        // Assert
+        Assert.AreEqual(expectedOutput, actualOutput);
+    }
+}
